d0464c46198abaca2ae50faf7c5bbe20ea0f38fa
Mark Tse <mark.tse@neverendingqs.com>
2018-01-22 22:35:57 -0500

    app - now only shows letters that have been guessed correctly; resolves #8.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 
+import currentWord from './currentWord';
 import lettersStatus from './lettersStatus';
 
-const rootReducer = combineReducers({ lettersStatus });
+const rootReducer = combineReducers({ currentWord, lettersStatus });
 export default rootReducer;
