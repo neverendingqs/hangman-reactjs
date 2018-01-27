6fe9e78313119e40af2e005fe9a7224fc2b81a55
Mark Tse <mark.tse@neverendingqs.com>
2018-01-27 10:26:15 -0500

    app - adding how many guesses are left; resolves #9.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Input from '../containers/Input';
+import Overview from '../containers/Overview';
 import Status from '../containers/Status';
 
 export default () =>
   <div>
     <Status />
+    <Overview />
     <Input />
   </div>;
---
+++ src/components/Overview.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class Overview extends Component {
+  getNumGuessesLeft() {
+    const lettersGuessed = Object
+      .keys(this.props.lettersStatus)
+      .filter(key => this.props.lettersStatus[key]);
+    
+    const numWrongGuesses = lettersGuessed.reduce(
+      (acc, letter) => this.props.currentWord.includes(letter)
+        ? acc
+        : acc + 1,
+      0
+    );
+
+    return this.props.maxGuesses - numWrongGuesses;
+  }
+
+  render() {
+    return <div className="row">
+      <h2>Guesses Left</h2>
+      {this.getNumGuessesLeft()}
+    </div>;
+  }
+}
+
+export default Overview;
