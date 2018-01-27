a3e918695b509b182f75539ea78dc5c23869dda0
Mark Tse <neverendingqs@users.noreply.github.com>
2018-01-27 14:24:07 -0500

    app - Diagram container now uses selectors. (#23)

--- src/components/Diagram.js
+++ src/components/Diagram.js
@@ -1,18 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Diagram extends Component {
-  maskedWord() {
-    return this.props.currentWord
-      .split('')
-      .map(x => this.props.lettersStatus[x] ? x : '_')
-      .join('');
-  }
-
-  render() {
-    return <div className="row">
-      <h2>{this.maskedWord()}</h2>
-    </div>;
-  }
-}
+const Diagram = ({ maskedWord }) =>
+  <div className="row">
+    <h2>{maskedWord}</h2>
+  </div>;
 
 export default Diagram;
